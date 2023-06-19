5f67f1014e5f915927bb372428d60899eefa3a8e
Asadbek Raimov <asadbek_raimov@epam.com>
2023-06-20 00:59:11 +0500

    feat: implemented cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const keys = args.filter(arg => arg.match(/--/));
+    const result = keys.map(key => {
+        const index = args.indexOf(key);
+        const value = args[index+1];
+        return `${key.replace('--','')} is ${value}`
+    })
+    console.table(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const args = process.env;
+    const varKeys = Object.keys(args);
+    const rssKeys = varKeys.filter(key => key.match(/RSS_/));
+    const result = rssKeys.map(key => `${key}=${args[key]}`);
+    console.table(result);
 };
 
 parseEnv();
