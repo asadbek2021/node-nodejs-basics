a9b75ed4487c9a13975acd687b1c3d5e44a3c2c8
Asadbek Raimov <asadbek_raimov@epam.com>
2023-06-20 00:35:30 +0500

    feat: fs list implemented

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+
 const list = async () => {
-    // Write your code here 
+   try {
+    const pathToFolder = path.join(__dirname, 'files');
+    const folderExists = fs.existsSync(pathToFolder);
+    
+    if(!folderExists) {
+        throw new Error('FS operation failed');
+    }
+
+    const files = await fs.promises.readdir(pathToFolder);
+    console.table(files);
+   } catch (err) {
+    throw err;
+   }
 };
 
 await list();
