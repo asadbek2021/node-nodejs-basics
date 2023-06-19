cc21ff0e10c4a2d15b15f2aeff4f496df5ed7aa0
Asadbek Raimov <asadbek_raimov@epam.com>
2023-06-20 01:57:26 +0500

    feat: streams implemented

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+   try{
+    const pathToFile = path.join(__dirname, 'files','fileToRead.txt');
+    const fileExists = fs.existsSync(pathToFile);
+
+    if(!fileExists){
+        throw new Error('FS operation failed');
+    }
+
+    const rs = fs.createReadStream(pathToFile);
+    rs.pipe(process.stdout);
+   } catch (err) {
+    throw err;
+   }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+
 const write = async () => {
-    // Write your code here 
+    try{
+        const pathToFile = path.join(__dirname, 'files','fileToWrite.txt');
+        const fileExists = fs.existsSync(pathToFile);
+    
+        if(!fileExists){
+            throw new Error('FS operation failed');
+        }
+    
+        const ws = fs.createWriteStream(pathToFile, {flags: 'a'});
+        process.stdin.pipe(ws);
+       } catch (err) {
+        throw err;
+       } 
 };
 
 await write();
