6ed129a43cd947ca7edfd43f0b6b9823670b89fd
Asadbek Raimov <asadbek_raimov@epam.com>
2023-06-20 00:22:11 +0500

    feat: fs rename implemented

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        const wrongFilePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+        const properFilePath = path.join(__dirname, 'files', 'properFilename.md');
+        const wrongFileExists = fs.existsSync(wrongFilePath);
+        const properFileExists = fs.existsSync(properFilePath);
+        
+        if(!wrongFileExists || properFileExists) {
+            throw new Error('FS operation failed')
+        }
+
+        await fs.promises.rename(wrongFilePath, properFilePath);
+    } catch (err) {
+        throw err;
+    }
 };
 
 await rename();
