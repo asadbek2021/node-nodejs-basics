4bd00fb3666a640886a1253fc7e0338e89c89451
Asadbek Raimov <asadbek_raimov@epam.com>
2023-06-19 23:50:41 +0500

    feat: fs create implemented

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from  'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+    try{
+        const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+        const fileExists = fs.existsSync(pathToFile);
+        if(fileExists) {
+            throw new Error('FS operation failed');
+        }
+        await fs.promises.writeFile(pathToFile, 'I am fresh and young', {flag: 'a+'})
+    } catch(err) {
+        throw err;
+    }
 };
 
 await create();
