8f992cd8b7a0e3ee1e36108e40c551dba643e6ca
Asadbek Raimov <asadbek_raimov@epam.com>
2023-06-20 01:20:11 +0500

    feat: hash implemented

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    try{ 
+        const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+        const fileExists = fs.existsSync(pathToFile);
+
+        if(!fileExists) {
+            throw new Error('FS operation failed');
+        }
+
+        const content = await fs.promises.readFile(pathToFile, {encoding: 'utf-8'});
+        const hash =  createHash('sha256').update(content).digest('hex');
+        console.log('Hash: ', hash);
+    } catch (err) {
+        throw err;
+    }
 };
 
 await calculateHash();
